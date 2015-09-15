6a97828efc87c881117f64c8fb9ec7c3ca8401a0
jmausolf <jgm346@gmail.com>
2015-09-15 12:39:19 -0500

    Updating years for graph data. Some years missing removed.

--- visualizations/6/make_folders.sh
+++ visualizations/6/make_folders.sh
@@ -9,175 +9,28 @@
 
 ##Make Year Folders
 
-mkdir	1940
-mkdir	1941
-mkdir	1942
-mkdir	1943
-mkdir	1944
-mkdir	1945
-mkdir	1946
-mkdir	1947
-mkdir	1948
-mkdir	1949
-mkdir	1950
-mkdir	1951
-mkdir	1952
-mkdir	1953
-mkdir	1954
-mkdir	1955
-mkdir	1956
-mkdir	1957
-mkdir	1958
-mkdir	1959
-mkdir	1960
-mkdir	1961
-mkdir	1962
-mkdir	1963
-mkdir	1964
-mkdir	1965
-mkdir	1966
-mkdir	1967
-mkdir	1968
-mkdir	1969
-mkdir	1970
-mkdir	1971
-mkdir	1972
-mkdir	1973
-mkdir	1974
-mkdir	1975
-mkdir	1976
-mkdir	1977
-mkdir	1978
-mkdir	1979
-mkdir	1980
-mkdir	1981
-mkdir	1982
-mkdir	1983
-mkdir	1984
-mkdir	1985
-mkdir	1986
-mkdir	1987
-mkdir	1988
-mkdir	1989
-mkdir	1990
-mkdir	1991
-mkdir	1992
-mkdir	1993
-mkdir	1994
-mkdir	1995
-mkdir	1996
-mkdir	1997
-mkdir	1998
-mkdir	1999
-mkdir	2000
-mkdir	2001
-mkdir	2002
-mkdir	2003
-mkdir	2004
-mkdir	2005
-mkdir	2006
-mkdir	2007
-mkdir	2008
-mkdir	2009
-mkdir	2010
-mkdir	2011
-mkdir	2012
-mkdir	2013
-mkdir	2014
-mkdir	2015
-mkdir 1940-2015
-mkdir time_lapse
+for year in	1941	1943	1946	1947	1948	1949	1951	1953	1955	1957	1958	1959	1960	1962	1963	1964	1965	1966	1967	1968	1969	1970	1971	1972	1973	1974	1975	1976	1977	1978	1979	1980	1981	1982	1983	1984	1985	1986	1987	1988	1989	1990	1991	1992	1993	1994	1995	1996	1997	1998	1999	2000	2001	2002	2003	2004	2005	2006	2007	2008	2009	2010	2011	2012	2013	2014	2015 1941-2015 time_lapse
+do
+  mkdir $year
+done
+
 
 ##Copy Website Assets and Master_Index HTML to Year Folders
 
 cd ..
 
-
-cp	-rf	6/assets	6/master_index.html	6/1940/
-cp	-rf	6/assets	6/master_index.html	6/1941/
-cp	-rf	6/assets	6/master_index.html	6/1942/
-cp	-rf	6/assets	6/master_index.html	6/1943/
-cp	-rf	6/assets	6/master_index.html	6/1944/
-cp	-rf	6/assets	6/master_index.html	6/1945/
-cp	-rf	6/assets	6/master_index.html	6/1946/
-cp	-rf	6/assets	6/master_index.html	6/1947/
-cp	-rf	6/assets	6/master_index.html	6/1948/
-cp	-rf	6/assets	6/master_index.html	6/1949/
-cp	-rf	6/assets	6/master_index.html	6/1950/
-cp	-rf	6/assets	6/master_index.html	6/1951/
-cp	-rf	6/assets	6/master_index.html	6/1952/
-cp	-rf	6/assets	6/master_index.html	6/1953/
-cp	-rf	6/assets	6/master_index.html	6/1954/
-cp	-rf	6/assets	6/master_index.html	6/1955/
-cp	-rf	6/assets	6/master_index.html	6/1956/
-cp	-rf	6/assets	6/master_index.html	6/1957/
-cp	-rf	6/assets	6/master_index.html	6/1958/
-cp	-rf	6/assets	6/master_index.html	6/1959/
-cp	-rf	6/assets	6/master_index.html	6/1960/
-cp	-rf	6/assets	6/master_index.html	6/1961/
-cp	-rf	6/assets	6/master_index.html	6/1962/
-cp	-rf	6/assets	6/master_index.html	6/1963/
-cp	-rf	6/assets	6/master_index.html	6/1964/
-cp	-rf	6/assets	6/master_index.html	6/1965/
-cp	-rf	6/assets	6/master_index.html	6/1966/
-cp	-rf	6/assets	6/master_index.html	6/1967/
-cp	-rf	6/assets	6/master_index.html	6/1968/
-cp	-rf	6/assets	6/master_index.html	6/1969/
-cp	-rf	6/assets	6/master_index.html	6/1970/
-cp	-rf	6/assets	6/master_index.html	6/1971/
-cp	-rf	6/assets	6/master_index.html	6/1972/
-cp	-rf	6/assets	6/master_index.html	6/1973/
-cp	-rf	6/assets	6/master_index.html	6/1974/
-cp	-rf	6/assets	6/master_index.html	6/1975/
-cp	-rf	6/assets	6/master_index.html	6/1976/
-cp	-rf	6/assets	6/master_index.html	6/1977/
-cp	-rf	6/assets	6/master_index.html	6/1978/
-cp	-rf	6/assets	6/master_index.html	6/1979/
-cp	-rf	6/assets	6/master_index.html	6/1980/
-cp	-rf	6/assets	6/master_index.html	6/1981/
-cp	-rf	6/assets	6/master_index.html	6/1982/
-cp	-rf	6/assets	6/master_index.html	6/1983/
-cp	-rf	6/assets	6/master_index.html	6/1984/
-cp	-rf	6/assets	6/master_index.html	6/1985/
-cp	-rf	6/assets	6/master_index.html	6/1986/
-cp	-rf	6/assets	6/master_index.html	6/1987/
-cp	-rf	6/assets	6/master_index.html	6/1988/
-cp	-rf	6/assets	6/master_index.html	6/1989/
-cp	-rf	6/assets	6/master_index.html	6/1990/
-cp	-rf	6/assets	6/master_index.html	6/1991/
-cp	-rf	6/assets	6/master_index.html	6/1992/
-cp	-rf	6/assets	6/master_index.html	6/1993/
-cp	-rf	6/assets	6/master_index.html	6/1994/
-cp	-rf	6/assets	6/master_index.html	6/1995/
-cp	-rf	6/assets	6/master_index.html	6/1996/
-cp	-rf	6/assets	6/master_index.html	6/1997/
-cp	-rf	6/assets	6/master_index.html	6/1998/
-cp	-rf	6/assets	6/master_index.html	6/1999/
-cp	-rf	6/assets	6/master_index.html	6/2000/
-cp	-rf	6/assets	6/master_index.html	6/2001/
-cp	-rf	6/assets	6/master_index.html	6/2002/
-cp	-rf	6/assets	6/master_index.html	6/2003/
-cp	-rf	6/assets	6/master_index.html	6/2004/
-cp	-rf	6/assets	6/master_index.html	6/2005/
-cp	-rf	6/assets	6/master_index.html	6/2006/
-cp	-rf	6/assets	6/master_index.html	6/2007/
-cp	-rf	6/assets	6/master_index.html	6/2008/
-cp	-rf	6/assets	6/master_index.html	6/2009/
-cp	-rf	6/assets	6/master_index.html	6/2010/
-cp	-rf	6/assets	6/master_index.html	6/2011/
-cp	-rf	6/assets	6/master_index.html	6/2012/
-cp	-rf	6/assets	6/master_index.html	6/2013/
-cp	-rf	6/assets	6/master_index.html	6/2014/
-cp	-rf	6/assets	6/master_index.html	6/2015/
-cp	-rf	6/assets	6/master_index.html	6/1940-2015
-cp	-rf	6/assets	6/master_index.html	6/time_lapse
+for year in	1941	1943	1946	1947	1948	1949	1951	1953	1955	1957	1958	1959	1960	1962	1963	1964	1965	1966	1967	1968	1969	1970	1971	1972	1973	1974	1975	1976	1977	1978	1979	1980	1981	1982	1983	1984	1985	1986	1987	1988	1989	1990	1991	1992	1993	1994	1995	1996	1997	1998	1999	2000	2001	2002	2003	2004	2005	2006	2007	2008	2009	2010	2011	2012	2013	2014	2015 1941-2015 time_lapse
+do
+  mkdir $year
+  cp	-rf	6/assets	6/master_index.html	6/$year/
+done
 
 cd 6/
 
 
 ##Rename Master Index in Each Year Folder
 
-for f in 1940	1941	1942	1943	1944	1945	1946	1947	1948	1949	1950	1951	1952	1953	1954	1955	1956	1957	1958	1959	1960	1961	1962	1963	1964	1965	1966	1967	1968	1969	1970	1971	1972	1973	1974	1975	1976	1977	1978	1979	1980	1981	1982	1983	1984	1985	1986	1987	1988	1989	1990	1991	1992	1993	1994	1995	1996	1997	1998	1999	2000	2001	2002	2003	2004	2005	2006	2007	2008	2009	2010	2011	2012	2013	2014	2015 1940-2015 time_lapse
+for f in	1941	1943	1946	1947	1948	1949	1951	1953	1955	1957	1958	1959	1960	1962	1963	1964	1965	1966	1967	1968	1969	1970	1971	1972	1973	1974	1975	1976	1977	1978	1979	1980	1981	1982	1983	1984	1985	1986	1987	1988	1989	1990	1991	1992	1993	1994	1995	1996	1997	1998	1999	2000	2001	2002	2003	2004	2005	2006	2007	2008	2009	2010	2011	2012	2013	2014	2015 1941-2015 time_lapse
 do
   cd $f
   mv master_index.html index.html
@@ -186,4 +39,4 @@ do
 
 done
 
-#cp -f master_index.html index.html
+cp -f master_index.html index.html
